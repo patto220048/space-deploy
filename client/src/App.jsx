236cf3749397061b53b11747d6abcc310f9941fc
patto220048 <220048xn@yse-c.net>
2023-04-12 23:58:51 +0900

    .dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,12 +31,12 @@ function App() {
   const [openRightbar, setOpenRightbar] = useState(false)
   const dispatch = useDispatch()
   
-  const socketio = useRef(io('https://api.space-social.online/api' ,{
+  const socketio = useRef(io('http://localhost:4000' ,{
     autoConnect: false
  }) )
   
  useEffect(()=> {
-  socketio.current = (io('https://api.space-social.online/api'))
+  socketio.current = (io('http://localhost:4000'))
 },[])
 
  useEffect(()=>{
