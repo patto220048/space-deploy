0f4eec083bc36eb84a1bcade645e48821297280a
patto220048 <220048xn@yse-c.net>
2023-06-27 21:29:04 +0900

    fix v.1

--- sever/src/db/index.js
+++ sever/src/db/index.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose').set('strictQuery', false)
 async function connect () {
 
     try{
-    PORT = 4000
-    PORT = 4000
         await mongoose.connect(process.env.MONGODB_URL);
         console.log("sever connect success");
     } catch (error){
