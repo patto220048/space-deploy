21c5c99e1103e6629b4f66ca9be5b9739cf74a64
patto220048 <220048xn@yse-c.net>
2023-06-29 00:54:38 +0900

    fix bug v2.1

--- sever/src/index.js
+++ sever/src/index.js
@@ -23,14 +23,12 @@ app.use(morgan("combined"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded());
-// app.use(function (req, res, next) {
-//     //Enabling CORS
-//     res.header('Access-Control-Allow-Credentials', true);
-//     // res.header("Access-Control-Allow-Origin", "*")
-//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-//     next();
-//     });
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+    });
 
 // connect to database
 const db = require("./db");
