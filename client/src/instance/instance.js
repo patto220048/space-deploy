5af3c0f1ebe19e6f0f9ebd99dd5ce673538dd174
patto220048 <220048xn@yse-c.net>
2023-06-30 21:45:16 +0900

    fix ssl

--- client/src/instance/instance.js
+++ client/src/instance/instance.js
@@ -1,11 +1,12 @@
-import axios from "axios"
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL : process.env.REACT_APP_API_URL || process.env.REACT_APP_API_URL_SSL,
+    baseURL: process.env.REACT_APP_API_URL_SSL ? process.env.REACT_APP_API_URL_SSL : process.env.REACT_APP_API_URL_SSL,
     withCredentials: true,
     headers: {
-        "Content-type": "application/json",
+        'Content-type': 'application/json',
     },
-})
+});
 
 export default axiosInstance;
+    
